783843e6860c0ae7c6ddc5c22a69d7a088b26a05
SammyLeths <s.o.eyiowuawi@gmail.com>
2023-06-30 21:05:31 +0100

    project prepped for deployment

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic';
+
 import getCurrentUser from './actions/getCurrentUser';
 import getListings, { IListingsParams } from './actions/getListings';
 import ClientOnly from './components/ClientOnly';
