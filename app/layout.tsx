a7c39648db3fee71f72f97cb92158841887d0c24
SammyLeths <s.o.eyiowuawi@gmail.com>
2023-09-20 17:11:32 +0100

    added metadat for SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,22 @@
-import './globals.css';
-import { Nunito } from 'next/font/google';
-import Navbar from './components/navbar/Navbar';
-import ClientOnly from './components/ClientOnly';
-import RegisterModal from './components/modals/RegisterModal';
-import LoginModal from './components/modals/LoginModal';
-import ToasterProvider from './providers/ToasterProvider';
-import getCurrentUser from './actions/getCurrentUser';
-import RentModal from './components/modals/RentModal';
-import SearchModal from './components/modals/SearchModal';
-import Footer from './components/footer/Footer';
+import "./globals.css";
+import { Nunito } from "next/font/google";
+import Navbar from "./components/navbar/Navbar";
+import ClientOnly from "./components/ClientOnly";
+import RegisterModal from "./components/modals/RegisterModal";
+import LoginModal from "./components/modals/LoginModal";
+import ToasterProvider from "./providers/ToasterProvider";
+import getCurrentUser from "./actions/getCurrentUser";
+import RentModal from "./components/modals/RentModal";
+import SearchModal from "./components/modals/SearchModal";
+import Footer from "./components/footer/Footer";
 
 export const metadata = {
-  title: 'Condo Rentals',
-  description: 'Condo Rentals Real Estate Listings',
+  title: "Condo Rentals",
+  description: "Condo Rentals Real Estate Listings",
 };
 
 const font = Nunito({
-  subsets: ['latin'],
+  subsets: ["latin"],
 });
 
 export default async function RootLayout({
@@ -26,7 +26,7 @@ export default async function RootLayout({
 }) {
   const currentUser = await getCurrentUser();
   return (
-    <html lang='en'>
+    <html lang="en">
       <body className={font.className}>
         <ClientOnly>
           <ToasterProvider />
@@ -36,7 +36,7 @@ export default async function RootLayout({
           <RegisterModal />
           <Navbar currentUser={currentUser} />
         </ClientOnly>
-        <div className='pb-20 pt-28'>{children}</div>
+        <div className="pb-20 pt-28">{children}</div>
         <Footer />
       </body>
     </html>
